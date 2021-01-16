713a7c4c39c679788ecddcafb616f103c7c3fc32
Bogdan Petru Pintican <bogdanpintican@gmail.com>
2021-01-16 21:32:01 +0200

    start work on hamburger menu

---
+++ packages/common/components/src/Icons/HamburgerMenu.tsx
@@ -0,0 +1,28 @@
+import { FunctionComponent } from 'react'
+
+const HamburgerMenu: FunctionComponent = () => (
+  <svg
+    width="23"
+    height="23"
+    viewBox="0 0 23 23"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M0 3C0 3 14.0179 3 23 3V6H0V3Z" fill="#124551" />
+    <path d="M0 10C0 10 14.0179 10 23 10V13H0V10Z" fill="#124551" />
+    <path d="M0 17C0 17 14.0179 17 23 17V20H0V17Z" fill="#124551" />
+  </svg>
+)
+
+{
+  /* <path
+d="M2.12134 0C2.12134 0 14.4655 12.3442 22.3752 20.2538L20.2538 22.3751L1.75834e-05 2.12132L2.12134 0Z"
+fill="#124551"
+/>
+<path
+d="M0 20.2538C0 20.2538 12.3442 7.90961 20.2538 -1.36492e-06L22.3751 2.12132L2.12132 22.3751L0 20.2538Z"
+fill="#124551"
+/> */
+}
+
+export default HamburgerMenu
