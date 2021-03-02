bb9a6873c46ebe4cbdd199c63fb69775f6ea6365
Bogdan Petru Pintican <bogdanpintican@gmail.com>
2021-03-02 22:59:13 +0200

    add some more tests for security rules

--- packages/common/api/api.test.ts
+++ packages/common/api/api.test.ts
@@ -2,22 +2,32 @@ import {
   initializeTestApp,
   assertSucceeds,
   assertFails,
+  clearFirestoreData,
+  initializeAdminApp,
 } from '@firebase/rules-unit-testing'
 
-const appId = 'pomodoro-like-app'
+const PROJECT_ID = 'pomodoro-like-app'
+const userId = 'some-id'
+const db = initializeTestApp({
+  projectId: PROJECT_ID,
+  auth: {
+    uid: userId,
+  },
+}).firestore()
+const dbAdmin = initializeAdminApp({
+  projectId: PROJECT_ID,
+}).firestore()
+
+const getProjectsDoc = () =>
+  db.collection('users').doc(userId).collection('projects')
+
+afterAll(() => {
+  clearFirestoreData({
+    projectId: PROJECT_ID,
+  })
+})
 
 describe('api', () => {
-  const userId = 'some-id'
-  const db = initializeTestApp({
-    projectId: appId,
-    auth: {
-      uid: userId,
-    },
-  }).firestore()
-
-  const getProjectsDoc = () =>
-    db.collection('users').doc(userId).collection('projects')
-
   it('Can read from own collection', async () => {
     await assertSucceeds(getProjectsDoc().doc('some-project').get())
   })
@@ -29,16 +39,46 @@ describe('api', () => {
     )
   })
 
-  it('project has a `name` string property', async () => {
-    await assertSucceeds(
-      getProjectsDoc().doc('project-id').set({
+  it.each([
+    ['name', 'string', 3],
+    ['strawberrySize', 3, 'string'],
+    ['breakSize', 3, 'string'],
+    ['description', 'string', 3],
+  ])('Can set %s as %s but not as %s', async (name, value, nonValidType) => {
+    await dbAdmin
+      .collection('users')
+      .doc(userId)
+      .collection('projects')
+      .doc('project-id')
+      .set({
         name: 'string',
+        strawberrySize: 0,
+        breakSize: 0,
+        description: 'description',
       })
+
+    await assertSucceeds(
+      getProjectsDoc()
+        .doc('project-id')
+        .set({
+          [name]: value,
+        })
     )
 
-    // await assertFails(
-    //   getProjectsDoc().doc('project-id').set({
-    //     name: 23,
-    //   })
+    await assertFails(
+      getProjectsDoc()
+        .doc('project-id')
+        .set({
+          [name]: nonValidType,
+        })
+    )
+  })
+
+  it("Can't update project-valid properties", async () => {
+    await assertFails(
+      getProjectsDoc().doc('project-id').set({
+        'non-valid-prop': 'not-valid',
+      })
+    )
   })
 })
